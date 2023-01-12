cfec3ce3d5d706248414648691ac4851bcc3345a
Toofi <herpoel.quentin@gmail.com>
2023-01-12 15:39:32 +0100

    deleted string message handling in slash commands handling discord bot

---
+++ Spotify.New.Releases.Application/Handlers/DiscordBotCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.Hosting;
+
+namespace Spotify.New.Releases.Application.Handlers
+{
+    public class DiscordBotCommandHandler : IHostedService
+    {
+        private readonly DiscordSocketClient _client;
+
+        public DiscordBotCommandHandler(DiscordSocketClient client)
+        {
+            this._client = client;
+            this._client.SlashCommandExecuted += SlashCommandHandler;
+
+        }
+
+        public async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            switch (command.Data.Name)
+            {
+                case "latest":
+                    await command.RespondAsync($"You executed {command.Data.Name} and you received will receive the latest release we have stored");
+                    break;
+                case "hello":
+                    await command.RespondAsync("Hello, I am the New Spotify Releases bot. I'm here to gather all the latest releases worldwide in Spotify, compare and store them.");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
